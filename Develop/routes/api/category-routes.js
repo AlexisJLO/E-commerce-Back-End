767aec0aad63e44bbf753e4a74f41645b5a67138
Alexis Lopez <lopez111alex@gmail.com>
2024-01-16 12:58:07 -0800

    fixed routing issues for POST, PUT, and DELETE

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -33,9 +33,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    const categoryData = await Category.create({
-      category_id: req.body.category_id,
-    });
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(404).json(err);
@@ -62,15 +60,8 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   try {
     const categoryData = await Category.destroy({
-      include: [{ model: Category }, { model: Product }],
-      attributes: {
-        include: [
-          [
-            sequelize.literal(
-              "(DELETE * FROM category WHERE category.product_id = product_id)"
-            ),
-          ],
-        ],
+      where: {
+        id: req.params.id,
       },
     });
 
